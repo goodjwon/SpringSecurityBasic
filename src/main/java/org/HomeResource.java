d6e8d469e7098a0acf05be5eea4e6fa0b3ba2953
goodjwon <firstman9@protonmail.com>
2020-08-21 08:09:59 +0900

    first commit

---
+++ src/main/java/org/HomeResource.java
@@ -0,0 +1,24 @@
+package org;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String home(){
+        return ("<h1>welcom</h1>");
+    }
+
+    @GetMapping("/user")
+    public String uesr(){
+        return ("<h1>welcom</h1>");
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return ("<h1>welcom</h1>");
+    }
+}
