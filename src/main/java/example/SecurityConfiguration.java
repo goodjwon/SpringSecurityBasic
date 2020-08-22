73871ca36b2faf5a92994c646b57908ec087e4d2
goodjwon <jwon76@naver.com>
2020-08-22 09:12:19 +0900

    기본 인증 처리 연습

---
+++ src/main/java/example/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package example;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //set my configuration on the auth object
+        auth.inMemoryAuthentication()
+                .withUser("goodjwon")
+                .password("pass")
+                .roles("USER")
+                .and()
+                .withUser("foo")
+                .password("foo")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();   //Do not use real application...
+    }
+}
